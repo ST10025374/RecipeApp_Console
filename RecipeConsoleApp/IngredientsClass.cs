fedf2c04dcc451cd8672d261582b05f7f5e423d7
Miguel Almeida <ST10025374@vcconnect.edu.za>
2023-04-26 16:40:07 +0200

    Class called IngredientsClass created, Ingredient variables from RecipeClass moved to IngredientClass, created instance of IngredientsClass to call methods from it, modified GetRecipe method to incorporate new changes. Array created to store ingredients details for each ingredient.

---
+++ RecipeConsoleApp/IngredientsClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeConsoleApp
+{
+    public class IngredientsClass
+    {
+        /// <summary>
+        /// Store the number of ingredients of each recipe
+        /// </summary>
+        private int NumberOfIngredients { get; set; } = 0;
+
+        /// <summary>
+        /// Store Ingredient Name
+        /// </summary>
+        private string IngredientName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Store Ingredient Quantity
+        /// </summary>
+        private double IngredientQuantity { get; set; } = 0.0;
+
+        /// <summary>
+        /// Store Ingredient Unit Of Measurement
+        /// </summary>
+        private string UnitOfMeasurement { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public IngredientsClass() 
+        { 
+        
+        }
+    }
+}
--- RecipeConsoleApp/RecipeClass.cs
+++ RecipeConsoleApp/RecipeClass.cs
@@ -15,26 +15,6 @@ namespace RecipeConsoleApp
         private string RecipeName { get; set; } = string.Empty;
 
         /// <summary>
-        /// Store the number of ingredients of each recipe
-        /// </summary>
-        private int NumberOfIngredients { get; set; } = 0;
-
-        /// <summary>
-        /// Store Ingredient Name
-        /// </summary>
-        private string IngredientName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Store Ingredient Quantity
-        /// </summary>
-        private double IngredientQuantity { get; set; } = 0.0;
-
-        /// <summary>
-        /// Store Ingredient Unit Of Measurement
-        /// </summary>
-        private string UnitOfMeasurement { get; set; } = string.Empty;
-
-        /// <summary>
         /// Store the recipe number of steps
         /// </summary>
         private int NumberOfSteps { get; set; } = 0;
@@ -43,6 +23,16 @@ namespace RecipeConsoleApp
         /// Will store a description of each step in Recipe
         /// </summary>
         private string StepDescription { get; set; } = string.Empty;
+        
+        /// <summary>
+        /// Instance of Ingredients class
+        /// </summary>
+        public IngredientsClass Ingredients = new IngredientsClass();
+
+        /// <summary>
+        /// Array to store details of each ingredient in recipe
+        /// </summary>
+        public List<IngredientsClass> IngredientsArray = new List<IngredientsClass>(); 
 
         //---------------------------------------------------------------------------------------//
         /// <summary>
@@ -77,134 +67,7 @@ namespace RecipeConsoleApp
                 }
             } while (!Valid);
         }
-
-        //---------------------------------------------------------------------------------------//
-        /// <summary>
-        /// Ask User for Number of Ingredients in Recipe and Store Input
-        /// Keep asking till correct input requested is added
-        /// In case incorrect input is added will display error message
-        /// </summary>
-        /// <param name="NumberOfIngredients"></param>
-        public void GetNumberOfIngredients()
-        {
-            Boolean Valid = true;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("Enter the amount of ingredients in the recipe: ");
-                    this.NumberOfIngredients = int.Parse(Console.ReadLine()); 
-                }
-                catch (FormatException)
-                {
-                    Valid = false;
-                    Console.WriteLine("Sorry, you did not enter a valid number. Please try again.");
-                }
-            } while (!Valid);
-        }
-
-        //---------------------------------------------------------------------------------------//
-        /// <summary>
-        /// Ask User for Ingredient Name in Recipe and Store Input
-        /// Keep asking till correct input requested is added
-        /// In case incorrect input is added will display error message
-        /// </summary>
-        public void GetIngredientName()
-        {
-            Boolean Valid = true;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("Enter the name of the ingredient: ");
-                    this.IngredientName = Console.ReadLine();
-                }
-                catch (FormatException)
-                {
-                    Valid = false;
-                    Console.WriteLine("Sorry, you did not enter a valid name. Please try again.");
-                }
-            } while (!Valid);
-        }
-
-        //---------------------------------------------------------------------------------------//
-        /// <summary>
-        /// Ask User for Ingredient Quantity and Store Input
-        /// Keep asking till correct input requested is added
-        /// In case incorrect input is added will display error message
-        /// </summary>
-        public void GetIngredientQuantity()
-        {
-            Boolean Valid = true;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("Enter the quantity of the ingredient: ");
-                    this.IngredientQuantity = double.Parse(Console.ReadLine());
-                }
-                catch (FormatException)
-                {
-                    Valid = false;
-                    Console.WriteLine("Sorry, you did not enter a valid number. Please try again.");
-                }
-            } while (!Valid);
-        }
-
-        //---------------------------------------------------------------------------------------//
-        /// <summary>
-        /// Ask User for Unit of Measurement and Record
-        /// User can select from 3 options
-        /// Method stores string of unit of measurement selected by user
-        /// Keep asking till correct input requested is added
-        /// In case incorrect input is added will display error message
-        /// </summary>
-        public void GetUnitOfMeasurement()
-        {
-            Boolean Valid = true;
-
-            int Option = 0;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("Select the unit of measurement by typing the option number: \n" +
-                              "[1] Gram (g) \n" +
-                              "[2] Tablespoon (tbsp) \n" +
-                              "[3] Cup (c)");
-
-                    Option = int.Parse(Console.ReadLine());
-
-                    if(Option < 1 || Option > 3)
-                    {
-                        Valid = false;
-                    }
-                }
-                catch (FormatException)
-                {
-                    Valid = false;
-                    Console.WriteLine("Sorry, you did not select a valid option. Please try again.");
-                }
-            } while (!Valid);
-          
-            switch (Option)
-            {
-                case 1:
-                    this.UnitOfMeasurement = "grams";
-                    break;                  
-                case 2:
-                    this.UnitOfMeasurement = "tablespoons";
-                    break;
-                case 3:
-                    this.UnitOfMeasurement = "cups";
-                    break;
-            }         
-        }
-
+      
         //---------------------------------------------------------------------------------------//
         /// <summary>
         /// Ask User for recipe steps amount
@@ -257,19 +120,6 @@ namespace RecipeConsoleApp
 
         //---------------------------------------------------------------------------------------//
         /// <summary>
-        /// Ask User for Ingredients Details calling methods to get Inputs
-        /// </summary>
-        public void GetIngredientDetails()
-        {
-            Console.WriteLine("\nType Ingredient Details" +
-                              "\n-----------------------\n");
-            GetIngredientName();
-            GetUnitOfMeasurement();
-            GetIngredientQuantity();
-        }
-
-        //---------------------------------------------------------------------------------------//
-        /// <summary>
         /// Method that calls methods 
         /// Asks user for Number of Ingredients and call method to get an ingredient detail
         /// Will loop for the number of ingredient specified
@@ -287,14 +137,18 @@ namespace RecipeConsoleApp
             //-----> Add Color to text <-----//
             
             GetRecipeName();
-            GetNumberOfIngredients();
+            Ingredients.GetNumberOfIngredients();
 
-            for (int i = 0; i < this.NumberOfIngredients; i++)
+            for (int i = 0; i < Ingredients.NumberOfIngredients; i++)
             {
-                //-- Add ingredient details to object to be stored in Array --//
+                IngredientsClass NewIngredient = new IngredientsClass();
+
                 Console.WriteLine("\nIngredient " + (i + 1) + 
                                   "\n------------\n");
-                GetIngredientDetails();
+
+                NewIngredient.GetIngredientDetails();
+
+                IngredientsArray.Add(NewIngredient);
             }
 
             Console.WriteLine("\nType number of Steps" +
